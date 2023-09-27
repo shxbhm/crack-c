b7b5e2b3b61e3836f6f453cb6df574e6ccc75938
Shubhamchakraborty001 <145741930+Shubhamchakraborty001@users.noreply.github.com>
2023-09-27 19:35:13 +0530

    projects

---
+++ name,email ID,approx age.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    char n,p,e;
+    int b;
+    printf("ENTER YOUR NAME:");
+    scanf("%s",&n);
+    printf("\n enter programme:");
+    scanf("%S",&p);
+    printf("\n enter birth year:");
+    scanf("%d"&b);
+    printf("calculated age:%d",2023-b);
+}
