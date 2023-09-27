7d1027d152a3ecc44d62fb79626a011f25368488
Shubhamchakraborty001 <145741930+Shubhamchakraborty001@users.noreply.github.com>
2023-09-27 16:56:29 +0530

    !

---
+++ c programmes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("enter two number:");
+    scanf("%d%d"&a,&b);
+    printf("\n sum=%d",a+b);
+    printf("\n difference=%d",a-b);
+    printf("\n product=%d",a*b);
+    printf("\n division=%f",a/b);
+}
