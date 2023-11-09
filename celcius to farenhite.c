ca44af2f74ebc0c7627dee55fdffd2d13ae7f070
shxbhm <chakrabortybasudeb04@gmail.com>
2023-11-10 00:14:33 +0530

    !

--- celcius to farenhite.c
+++ celcius to farenhite.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     float c,f;
-    printf("Enter temparature in celcius:");
+    printf("Enter temparature in celsius:");
     scanf("%f",&c);
     f=(c*9/5)+32;
     printf("temparature in farenhite:%f",f);
